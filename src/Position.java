1300185d90110e3580554e02941308ff9bd8d4ca
4010e2 <4010e2@mandarina.ece.uprm.edu>
2018-08-29 16:45:19 -0400

    Position done ;)

--- src/Position.java
+++ src/Position.java
@@ -67,9 +67,14 @@ public class Position {
 	 */
 	public double getYearlySalary() {
 		
-		//Add Code Here [You have to use If/Else to get graded]
 		
-		return 0; //Temporal Return
 		
+		if(this.jobType == true){
+			
+			return  this.hSalary*40*4.5*12;
+		}
+		else{
+			return this.hSalary*20*4.5*12;
+		}
 	}
 }
